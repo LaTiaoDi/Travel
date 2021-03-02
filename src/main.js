cad2c5476b27e69d3b1d13b81fe078b4d19a5908
latiaodi <2322743837@qq.com>
2021-03-02 09:37:37 +0800

    轮播图

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
 import 'styles/reset.css'
-
+Vue.use(VueAwesomeSwiper)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+   <div> <swiper ref="mySwiper" :options="swiperOptions">
+    <swiper-slide v-for="item of swiperList" :key='item.id'><img :src="item.imgUrl"></swiper-slide>
+
+  
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+	</div>
+</template>
+
+<script>
+ export default {
+    name: 'HomeSwiper',
+    data() {
+      return {
+        swiperOptions: {
+          pagination: {
+            el: '.swiper-pagination'
+          },
+					loop:true
+          // Some Swiper option/callback...
+        },
+				swiperList:[{
+					id:'0001',
+					imgUrl:'//imgs.qunarzz.com/vs_ceph_vcimg/f03f5ac90ae59d0d9c6332a2bfd9782e.jpeg'
+				},{
+					id:'0002',
+					imgUrl:'//imgs.qunarzz.com/vs_ceph_vcimg/569cae4ae98e9793f7341e85eed73c54.jpeg'
+				}]
+      }
+    },
+    computed: {
+      swiper() {
+        return this.$refs.mySwiper.$swiper
+      }
+    },
+    mounted() {
+      console.log('Current Swiper instance object', this.swiper)
+      this.swiper.slideTo(3, 1000, false)
+    }
+  }
+</script>
+<style lang="stylus" scoped>
+	img {
+		width: 100%;
+		height:100%;
+	}
+	div >>> .swiper-pagination-bullet-active {
+		background-color: #fff !important
+	}
+</style>
