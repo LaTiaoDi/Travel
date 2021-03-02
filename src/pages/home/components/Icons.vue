f5a4cbd85bfa9d2386f5e71911de01680d679ef0
latiaodi <2322743837@qq.com>
2021-03-02 12:58:57 +0800

    icon

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="icons">
+        <div class="icon" v-for="item of iconList" :key='item.id'>
+            <div class="icon-img">
+                <img class="icon-img-content" :src="item.imgUrl" :alt="item.desc">
+            </div>
+            <p class="icon-desc">{{item.desc}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'icons',
+    data:function(){
+        return {
+            iconList: [
+                {
+                id:'001',
+                imgUrl:'//s.qunarzz.com/homenode/images/touchheader/train.png'
+                ,desc:'自由女神'
+                }, {
+                id:'002',
+                imgUrl:'//s.qunarzz.com/homenode/images/touchheader/train.png'
+                ,desc:'自由女神'
+                }, {
+                id:'003',
+                imgUrl:'//s.qunarzz.com/homenode/images/touchheader/train.png'
+                ,desc:'自由女神'
+                }, {
+                id:'004',
+                imgUrl:'//s.qunarzz.com/homenode/images/touchheader/train.png'
+                ,desc:'自由女神'
+                }, {
+                id:'005',
+                imgUrl:'//s.qunarzz.com/homenode/images/touchheader/train.png'
+                ,desc:'自由女神'
+                }, {
+                id:'006',
+                imgUrl:'//s.qunarzz.com/homenode/images/touchheader/train.png'
+                ,desc:'自由女神'
+                }, {
+                id:'007',
+                imgUrl:'//s.qunarzz.com/homenode/images/touchheader/train.png'
+                ,desc:'自由女神'
+                }, {
+                id:'008',
+                imgUrl:'//s.qunarzz.com/homenode/images/touchheader/train.png'
+              ,desc:'自由女神'
+              },
+            ]
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+@import '~styles/mixins.styl'
+  .icons >>> .swiper-container
+    height: 0
+    padding-bottom: 50%
+  .icons
+    margin-top: .1rem
+    .icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $darkTextColor
+        ellipsis()
+</style>
